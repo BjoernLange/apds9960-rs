1bb50b3e4353170196a7d497d93aeb9abe597b66
Domenic Quirl <domenicquirl@t-online.de>
2025-10-29 16:37:57 +0100

    test(examples): clean up unused import warnings on non-linux

--- examples/linux.rs
+++ examples/linux.rs
@@ -1,13 +1,12 @@
-use apds9960::Apds9960;
-#[cfg(target_os = "linux")]
-use linux_embedded_hal::I2cdev;
-use nb::block;
-
 #[cfg(not(target_os = "linux"))]
 fn main() {}
 
 #[cfg(target_os = "linux")]
 fn main() {
+    use apds9960::Apds9960;
+    use linux_embedded_hal::I2cdev;
+    use nb::block;
+
     let dev = I2cdev::new("/dev/i2c-1").unwrap();
     let mut sensor = Apds9960::new(dev);
     sensor.enable().unwrap();
